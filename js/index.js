2512b246ce5207713f5b9b5c03613f4618057bf6
Patricio Abarca <patricioabarca88@gmail.com>
2023-03-12 23:20:26 -0300

    I add the opacity

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 const $nav = document.querySelector('.nav');
 const $menu = document.querySelector('.header__menu');
 const $close = document.querySelector('.nav__close');
-
+const $opacity = document.querySelector('.opacity');
 
 $menu.addEventListener('click', (e)=>{
     $nav.classList.toggle('nav--on');
+    $opacity.classList.add('opacity-on');
 });
 
 $close.addEventListener('click',(e)=>{
     $nav.classList.toggle('nav--on');
+    $opacity.classList.remove('opacity-on');
 });
